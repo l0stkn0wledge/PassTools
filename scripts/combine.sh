064bf0d6d07954b73dcb586bfa945404b9a05f75
l0stkn0wledge <l0stkn0wledge@users.noreply.github.com>
2016-11-04 10:12:46 -0500

    Adding combine.sh
    This script takes the hashcat results and the original file containing usernames and maps them back together to username:password. It is designed to only do this for found accounts, so the result file will not be cluttered with uncracked hashes.

---
+++ scripts/combine.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+IFS=$'\t\n'
+
+if [ $# -ne 3 ]
+then
+    	echo "Usage: combine.sh [results file] [original file] [output file]"
+		echo ""
+		echo "If cracking multiple sets, you can use the pot file as the results file"
+    echo "if you have a large pot file, this may take a LONG time to run, so the"
+    echo "recommendation is to always use output files unique for each assessment."
+	exit -1
+fi
+
+# Create the array of hashes and the found passwords
+# The sed in PASSES should make those safe for search later
+HASHES=($(cut -d':' -f1 $1))
+PASSES=($(cut -d':' -f2 $1 |  sed -e 's/[\/&\s]/\\&/g'))
+
+# Generate the output file as a copy of the original
+# search for the found hashes and only copy out the
+# credential pairs that have found hashes.
+cut -d':' -f1 $1 | xargs -I {} -xn1 grep {} $2 > $3
+
+LEN=${#HASHES[@]}
+
+# For each found hash in the output file, substitute the hash
+# with the found password. This is done globally because users
+# may have the same weak passwords.
+for (( i=0 ; i < $LEN; i++ )); do
+	printf "\r $i of $LEN"
+#	SAFE_PASS=$(printf '%s\n' "${PASSES[i]}" |s sed -e 's/[\/&\s]/\\&/g')
+	sed -i "s/${HASHES[$i]}/${PASSES[i]}/g" $3
+done
+
+exit 0
